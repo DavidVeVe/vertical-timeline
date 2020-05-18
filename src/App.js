ba5c1a9e96970cc16fce03c918391b674106d0f8
David Velazquez <davidarmando.gdl@live.com>
2020-05-18 18:47:08 -0500

    [add] vertical line

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import classes from "./App.module.css";
+
+import VerticalTimeline from "./containers/VerticalTimeline/VerticalTimeline";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>App working!</p>
+        {/* VerticalTimeline recibe "isActive" como prop y que debe ser un objeto con las siguientes propiedades que tienen que ser booleanos: */}
+        {/* received, processing, ready, sent, delivered */}
+        <VerticalTimeline />
+      </div>
+    );
+  }
 }
 
 export default App;
